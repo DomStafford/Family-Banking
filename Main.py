e3584998cf9eb603439517cd0924aaafb4f6efc7
Dom Stafford <43580868+DomStafford@users.noreply.github.com>
2018-09-29 14:45:10 +0100

    Minor changes
    Added full stops, removed printing empty lines after asking which user is accessing it.

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
 from BankAccount import BankAccount
 from FunctionsInsideInterface import interact
+
 path = 'bank_accounts.txt'
 accounts = []
 with open(path, 'r') as f:
     for line in f:
         separated_line = line.split(':')
         accounts.append(BankAccount(separated_line[0], int(separated_line[1])))
-account_names = [account.get_name() for account in accounts]
 
 interact(accounts)
-
 print('Thank you for using the app. Updating all balances.')
 
 with open(path, 'w') as f:
